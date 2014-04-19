cc7d1ac3a1ed0641e3b0aca061a08c6ff314909d
Sam Symons <samsymons@me.com>
2014-04-18 18:48:52 -0700

    Add some git aliases.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,8 +2,14 @@ alias l='ls -lhoAF'
 alias reload='. ~/.zshrc'
 alias reload!='~/.dotfiles/script/install && reload'
 
-# Show/hide hidden files in Finder
-alias show="defaults write com.apple.Finder AppleShowAllFiles -bool true && killall Finder"
-alias hide="defaults write com.apple.Finder AppleShowAllFiles -bool false && killall Finder"
-
 alias tmux="TERM=screen-256color-bce tmux"
+
+# git
+alias gs="git status"
+alias gp="git pull --rebase && git push"
+alias gl="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(blue)<%an>%Creset' --abbrev-commit"
+alias gco="git checkout"
+
+# Bundler
+alias b="bundle"
+alias be="bundle exec"
