7befe227b58eef71a780a30f468b00e50055ac46
Sam Symons <sam@samsymons.com>
2014-05-05 13:19:47 -0700

    Use the short git status format with gs.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,7 +5,7 @@ alias reload!='~/.dotfiles/script/install && reload'
 alias tmux="TERM=screen-256color-bce tmux"
 
 # git
-alias gs="git status"
+alias gs="git status -s -b"
 alias gp="git pull --rebase && git push"
 alias gl="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(blue)<%an>%Creset' --abbrev-commit"
 alias gco="git checkout"
