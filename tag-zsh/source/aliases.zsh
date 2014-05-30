f6005fbf1a7008533359c2fc6e80b1fedec5bbb6
Sam Symons <sam@samsymons.com>
2014-05-30 13:12:53 -0700

    Add a migrate alias for Rails.

--- tag-zsh/source/aliases.zsh
+++ tag-zsh/source/aliases.zsh
@@ -4,8 +4,11 @@ alias reload!='rcup -d .dotfiles -v; . ~/.zshrc'
 
 alias tmux="TERM=screen-256color-bce tmux"
 
-# git
+# Git
 alias gs="git status -s -b"
 alias gp="git pull --rebase && git push"
 alias gl="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(blue)<%an>%Creset' --abbrev-commit"
 alias gco="git checkout"
+
+# Rails
+alias migrate="rake db:migrate db:rollback && rake db:migrate"
