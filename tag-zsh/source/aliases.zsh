ee7ad1bedcf175f67b5b467082452a79aad871c3
Sam Symons <sam@samsymons.com>
2014-06-26 09:48:10 -0700

    Add an alias for viewing merged branches.

--- tag-zsh/source/aliases.zsh
+++ tag-zsh/source/aliases.zsh
@@ -8,7 +8,7 @@ alias tmux="TERM=screen-256color-bce tmux"
 alias gs="git status -s -b"
 alias gp="git pull --rebase && git push"
 alias gl="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(blue)<%an>%Creset' --abbrev-commit"
-alias gco="git checkout"
+alias gm="git branch --merged | grep -v '\*'"
 
 # Rails
 alias migrate="rake db:migrate db:rollback && rake db:migrate"
