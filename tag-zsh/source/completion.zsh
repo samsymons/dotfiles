681a4e3198524de18b11f5edee865ee6d874edaf
Sam Symons <sam@samsymons.com>
2014-08-16 16:13:00 -0700

    Use ctags for ag completion.

--- tag-zsh/source/completion.zsh
+++ tag-zsh/source/completion.zsh
@@ -3,3 +3,11 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
 # Pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
+
+#compdef ag
+
+if (( CURRENT == 2 )); then
+  compadd $(cut -f 1 tmp/tags .git/tags 2>/dev/null)
+else;
+  _files
+fi
