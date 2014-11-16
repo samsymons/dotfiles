61ea238623a1d5bc550c3bf3de7a78ff267b346b
Sam Symons <sam@samsymons.com>
2014-11-15 21:16:15 -0800

    Remove the last traces of hub.

--- tag-git/source/git.zsh
+++ tag-git/source/git.zsh
@@ -1,9 +1,6 @@
 export GHI_PAGER=less
 export GIT_MERGE_AUTOEDIT=no
 
-# Wrap git with hub
-if [[ -f `command -v hub` ]] ; then alias git=hub ; fi
-
 function g {
     if [[ $# > 0 ]]; then
         git "$@"
