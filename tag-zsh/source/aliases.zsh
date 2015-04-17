0ef21f3f4851fb6215cbad81ba6cfd5c570b50ac
Sam Symons <sam@samsymons.com>
2015-04-16 23:53:44 -0700

    Overdue alias cleanup.

--- tag-zsh/source/aliases.zsh
+++ tag-zsh/source/aliases.zsh
@@ -5,9 +5,6 @@ alias path="echo $PATH | tr -s ':' '\n'"
 
 alias tmux="TERM=screen-256color-bce tmux"
 alias mkdir='mkdir -p'
-alias ...='../..'
-
-alias nis='npm install --save'
 
 # Git
 alias gl="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(blue)<%an>%Creset' --abbrev-commit"
@@ -15,14 +12,11 @@ alias gm="git branch --merged | grep -v '\*'"
 alias gpr="git pull-request -b develop"
 
 # iOS
-
 alias pi="pod install"
 alias po="pod outdated"
 
 # Rails
 alias be="bundle exec"
-alias bo="bundle oudated"
-alias m="migrate"
+alias bo="bundle outdated"
 alias migrate="rake db:migrate db:rollback && rake db:migrate"
-alias rk="rake"
 alias s="bundle exec rspec"
