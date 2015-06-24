a0518bbb04d52442a8af087019d2eb05a6a2537a
Sam Symons <sam@samsymons.com>
2015-06-24 07:51:07 -0700

    Remove old git aliases.

--- tag-zsh/source/aliases.zsh
+++ tag-zsh/source/aliases.zsh
@@ -7,6 +7,7 @@ alias tmux="TERM=screen-256color-bce tmux"
 alias mkdir='mkdir -p'
 
 # Git
+alias g="git"
 alias gl="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(blue)<%an>%Creset' --abbrev-commit"
 alias gm="git branch --merged | grep -v '\*'"
 alias gpr="git pull-request -b develop"
