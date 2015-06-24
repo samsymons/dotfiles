c2cda524d4996dcc69eb2035531a77a6c154ca66
Sam Symons <sam@samsymons.com>
2015-06-24 07:55:25 -0700

    Git alias overhaul.

--- tag-zsh/source/aliases.zsh
+++ tag-zsh/source/aliases.zsh
@@ -7,9 +7,9 @@ alias tmux="TERM=screen-256color-bce tmux"
 alias mkdir='mkdir -p'
 
 # Git
-alias g="git"
+alias gs="git status"
+alias gd="git diff --cached"
 alias gl="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(blue)<%an>%Creset' --abbrev-commit"
-alias gm="git branch --merged | grep -v '\*'"
 alias gpr="git pull-request -b develop"
 
 # iOS
