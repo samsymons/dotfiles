54267e4d4ac34f128b697b59f8f3af90e7530127
Sam Symons <sam@samsymons.com>
2017-11-18 19:43:21 -0800

    hyper.js update.

--- hyper.js
+++ hyper.js
@@ -8,51 +8,19 @@ module.exports = {
     // or "canary" for less polished but more frequent updates
     updateChannel: 'stable',
 
-    // default font size in pixels for all tabs
-    fontSize: 13,
-
-    // font family with optional fallbacks
-    fontFamily: '"Fira Code", Consolas, Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
-
-    // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
+    fontFamily: '"Fira Code", monospace',
     cursorColor: 'rgba(69, 217, 154, 0.75)',
-
-    // `BEAM` for |, `UNDERLINE` for _, `BLOCK` for █
     cursorShape: 'BLOCK',
-
-    // set to true for blinking cursor
     cursorBlink: false,
-
-    // color of the text
     foregroundColor: '#fff',
-
-    // terminal background color
     backgroundColor: '#000',
-
-    // border color (window, tabs)
     borderColor: '#333',
-
-    // custom css to embed in the main window
     css: '',
-
-    // custom css to embed in the terminal window
-    termCSS: '.xterm {line-height: 18px} .xterm-rows > div {display: block; line-height: 15px}',
-
-    // set to `true` (without backticks) if you're using a Linux setup that doesn't show native menus
-    // default: `false` on Linux, `true` on Windows (ignored on macOS)
+    termCSS: 'x-row {line-height: initial} .unicode-node {position: relative}',
     showHamburgerMenu: '',
-
-    // set to `false` if you want to hide the minimize, maximize and close buttons
-    // additionally, set to `'left'` if you want them on the left, like in Ubuntu
-    // default: `true` on windows and Linux (ignored on macOS)
     showWindowControls: '',
-
-    // custom padding (css format, i.e.: `top right bottom left`)
     padding: '14px 14px',
 
-    // the full list. if you're going to provide the full color palette,
-    // including the 6 x 6 color cubes and the grayscale map, just provide
-    // an array here instead of a color map object
     colors: {
       black       : '#000000',
       red         : '#e83a30', // red
@@ -71,48 +39,13 @@ module.exports = {
       lightCyan   : '#31A2FE', // cyan
     },
 
-    // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
-    // if left empty, your system's login shell will be used by default
-    //
-    // Windows
-    // - Make sure to use a full path if the binary name doesn't work
-    // - Remove `--login` in shellArgs
-    //
-    // Bash on Windows
-    // - Example: `C:\\Windows\\System32\\bash.exe`
-    //
-    // Powershell on Windows
-    // - Example: `C:\\WINDOWS\\System32\\WindowsPowerShell\\v1.0\\powershell.exe`
     shell: '/usr/local/bin/zsh',
-
-    // for setting shell arguments (i.e. for using interactive shellArgs: ['-i'])
-    // by default ['--login'] will be used
     shellArgs: ['--login'],
-
-    // for environment variables
     env: {},
-
-    // set to false for no bell
     bell: false,
-
-    // if true, selected text will automatically be copied to the clipboard
     copyOnSelect: false
-
-    // if true, on right click selected text will be copied or pasted if no
-    // selection is present (true by default on Windows)
-    // quickEdit: true
-
-    // URL to custom bell
-    // bellSoundURL: 'http://example.com/bell.mp3',
-
-    // for advanced config flags please refer to https://hyper.is/#cfg
   },
 
-  // a list of plugins to fetch and install from npm
-  // format: [@org/]project[#version]
-  // examples:
-  //   `hyperpower`
-  //   `@company/project`
-  //   `project#1.0.1`
   plugins: ['hyper-statusline', 'hyper-ligatures'],
+  localPlugins: ['hyper-pacific'],
 };
