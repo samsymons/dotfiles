b2a61e94ecff6194015bfcabef5b6961ed5fe3d4
Sam Symons <sam@samsymons.com>
2021-11-07 10:09:45 -0800

    Tweak brew.sh.

--- brew.sh
+++ brew.sh
@@ -11,15 +11,6 @@ brew upgrade
 # Save Homebrew’s installed location.
 BREW_PREFIX=$(brew --prefix)
 
-# Switch to using brew-installed bash as default shell
-if ! fgrep -q "${BREW_PREFIX}/bin/zsh" /etc/shells; then
-  echo "${BREW_PREFIX}/bin/zsh" | sudo tee -a /etc/shells;
-  chsh -s "${BREW_PREFIX}/bin/zsh";
-fi;
-
-# Install GnuPG to enable PGP-signing commits.
-brew install gnupg
-
 # Install more recent versions of some macOS tools.
 brew install neovim
 brew install openssh
@@ -29,8 +20,8 @@ brew install carthage
 brew install exa
 brew install fd
 brew install fzf
+brew install gh
 brew install git
-brew install git-lfs
 brew install rbenv
 brew install rename
 brew install reattach-to-user-namespace
@@ -41,3 +32,9 @@ brew install zsh
 
 # Remove outdated versions from the cellar.
 brew cleanup
+
+# Switch to using brew-installed zsh as default shell
+if ! fgrep -q "${BREW_PREFIX}/bin/zsh" /etc/shells; then
+  echo "${BREW_PREFIX}/bin/zsh" | sudo tee -a /etc/shells;
+  chsh -s "${BREW_PREFIX}/bin/zsh";
+fi;
